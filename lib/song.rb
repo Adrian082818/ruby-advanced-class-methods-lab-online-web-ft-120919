0929e322cf8c3867c3edf53dad969bbc46bc5ddb
Adrian Mohabir <mohabiradrian@gmail.com>
2019-12-21 15:05:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -46,7 +46,7 @@ def self.new_from_filename(filename)
   song
 end 
 
-def self.create_from_filename
+def self.create_from_filename(filename)
   self.new_from_filename(filename).tap { |song| song.save }
 end 
 
