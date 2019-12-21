de513ba13b2ac66b4e4c4bc9ecefafac244ae1c9
Adrian Mohabir <mohabiradrian@gmail.com>
2019-12-21 14:27:52 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,4 +26,8 @@ def self.find_by_name(name)
   # self.create_by_name(name).tap {|song| song.name == name }
   self.all.find {|song| song.name == name }
 end 
+
+def self.find_or_create_by_name(name)
+  
+end 
 end
